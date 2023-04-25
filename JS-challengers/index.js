8030ea10ffceff5f62e44856b8fa4176386764f4
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-04-25 21:04:24 +0100

    JS - challengers - Reverse String

---
+++ JS-challengers/index.js
@@ -0,0 +1,6 @@
+function ReverseString(string) {
+    return string.split('').reverse().join('');
+}
+
+console.log(ReverseString('123456')); // => 654321
+console.log(ReverseString('zouhair')); // => riahuoz
